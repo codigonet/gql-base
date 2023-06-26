82d949140972bc75009252fff5e9defcfe6854b4
Cristian Acuña <cas@codigonet.cloud>
2023-06-26 12:21:46 -0400

    intentional syntax error

--- src/gql/resolvers/additional/index.ts
+++ src/gql/resolvers/additional/index.ts
@@ -3,6 +3,8 @@ import { Additional } from "~services/additional";
 export const AdditionalResolver = {
   Query: {
     additional: () => {
+      const password = "clave-fija"
+      def algo: "error de código"
       const response = {
         message: Additional.message(),
         run_at: Additional.run_at(),
